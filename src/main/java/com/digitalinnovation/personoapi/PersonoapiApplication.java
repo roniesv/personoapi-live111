f653279f50ebb7d9e8f36733231c97623b3f9c6f
ronsv <ronie.souzavieira@gmail.com>
2021-09-07 21:44:07 -0300

    estrutura inicial

---
+++ src/main/java/com/digitalinnovation/personoapi/PersonoapiApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.personoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonoapiApplication.class, args);
+	}
+
+}
